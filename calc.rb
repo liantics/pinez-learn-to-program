ad5342a533a3896d6c1461264d0fae6667a47b52
liantics <liantics.antics@gmail.com>
2014-05-12 13:43:08 -0400

    part 1 of more about methods

--- calc.rb
+++ calc.rb
@@ -1,27 +1,34 @@
-var1 = 2
-var2 = '5'
-
-puts var1.to_s + var2
-puts var1 + var2.to_i
-
+puts 'hello'.+ ' world'
+puts (10.*9).+ 9
 puts ''
 
-puts '15'.to_f
-puts '99.999'.to_f
-puts '99.999'.to_i
-puts ''
-puts '5 is my favorite number!'.to_i
-puts 'Who asked you about 5 or whatever?'.to_i
-puts 'Your momma did'.to_f
-puts ''
-puts 'stringy'.to_s
-puts 3.to_i
-puts ''
-puts 'enter a string:'
-puts gets
+iCantBelieveIMadeAVariableNameThisLongJustToPointToA3 = 3
+puts 'puts'
+puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3
+puts 'self.puts'
+#not working in this version of ruby - self.puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3 
 
-puts ''
-puts 'Hello there, and what\'s your name?'
+var1 = 'stop'
+var2 = 'stressed'
+var3 = 'Can you pronounce this sentence backwards?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+puts 'What is your full name?'
 name = gets.chomp
-puts 'Your name is ' +name+ '? What a lovely name!'
-puts 'Pleased to meet you, ' +name+ '. :-)'
+puts 'Did you know there are ' +name.length.to_s + ' characters in your name, ' + name + '?'
+
+puts ''
+puts 'First name?'
+first = gets.chomp
+puts 'Middle name?'
+middle = gets.chomp
+puts 'Last name?'
+last = gets.chomp
+concat = first.length + middle.length + last.length
+puts 'Did you know there are ' + concat.to_s + ' letters in ' + first + ' ' + middle + ' ' + last + '?'
