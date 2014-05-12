c9c61d782e1e79db069b95eecbbda3dc27b5db1a
liantics <liantics.antics@gmail.com>
2014-05-12 10:13:58 -0400

    numbers exercise

--- calc.rb
+++ calc.rb
@@ -1,4 +1,24 @@
-puts 1.0 + 2.0
-puts 2.0 * 3.0
-puts 5.0 - 8.0
-puts 9.0/2.0
+puts 5* (12-8) + -15
+puts 98 + (59872 / (13*8)) * -52
+
+puts "hours per year"
+
+days = 365
+hours = 24
+hyear = days * hours
+puts hyear
+
+puts "how many minutes are in a decade?"
+hdecade = hyear * 10
+mdecade = hdecade * 60
+puts mdecade
+
+
+puts "how many seconds old are you?"
+age = 49
+sAge = age * hyear * 60 * 60
+
+puts sAge
+
+puts "how many chocolates do you hope to eat in your life? "
+puts "a LOT!"
