bbe7b162d3fe533e4da519417647a17c28e350a4
liantics <liantics.antics@gmail.com>
2014-05-12 10:24:19 -0400

    letters exercise

--- calc.rb
+++ calc.rb
@@ -1,24 +1,38 @@
-puts 5* (12-8) + -15
-puts 98 + (59872 / (13*8)) * -52
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye.'
+puts ''
+puts 'random text'
+puts '... \'cuz random is cool'
+puts ''
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie'
+puts ''
+puts 'blink ' * 4
+puts ''
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts ''
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+puts ''
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
 
-puts "hours per year"
-
-days = 365
-hours = 24
-hyear = days * hours
-puts hyear
-
-puts "how many minutes are in a decade?"
-hdecade = hyear * 10
-mdecade = hdecade * 60
-puts mdecade
 
 
-puts "how many seconds old are you?"
-age = 49
-sAge = age * hyear * 60 * 60
 
-puts sAge
 
-puts "how many chocolates do you hope to eat in your life? "
-puts "a LOT!"
+
+
+
+
+
+
+
+
